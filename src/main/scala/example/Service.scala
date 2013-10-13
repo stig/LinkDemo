ff57ec072bf2d0e2b386add4fc7ed2cc58bd2ad1
Stig Brautaset <stig@brautaset.org>
2013-10-13 23:54:33 +0100

    Add 5 second internal cache for the 1000 most recent GET requests
    This makes the server able to handle 22,000+ requests / second on my Macbook Air:
    
    % wrk -t 5 -c 50 'http://localhost:8080/by-offset'
    Running 10s test @ http://localhost:8080/by-offset
      5 threads and 50 connections
      Thread Stats   Avg      Stdev     Max   +/- Stdev
        Latency     4.47ms   16.03ms 122.01ms   97.68%
        Req/Sec     4.79k     1.24k    9.07k    83.03%
      221562 requests in 10.00s, 113.47MB read
    Requests/sec:  22154.16
    Transfer/sec:     11.35MB

--- src/main/scala/example/Service.scala
+++ src/main/scala/example/Service.scala
@@ -14,6 +14,7 @@ import spray.http.CacheDirectives.`max-age`
 import spray.http.Uri
 import spray.httpx.SprayJsonSupport._
 import akka.pattern.ask
+import spray.routing.directives.CachingDirectives._
 
 class Service(model: ActorRef) extends HttpServiceActor with ItemJsonProtocol {
   import context.dispatcher
@@ -29,25 +30,30 @@ class Service(model: ActorRef) extends HttpServiceActor with ItemJsonProtocol {
   def linkHeader(next: Uri) =
     RawHeader("Link", s"""<$next>; rel="next"""")
 
+  // A 5-second cache for 1000 most recent GET requests
+  val simpleCache = routeCache(maxCapacity = 1000, timeToLive = 5.seconds)
+
   def receive = runRoute(
-    get {
-      path(JavaUUID) { id =>
-        onSuccess(model ? id) {
-          case i: Item => complete(OK, cacheHeader(i.cache) :: Nil, i)
-          case None => complete(NotFound)
-        }
-      } ~
-        path("by-offset") {
-          parameters('offset ? 0, 'limit ? 3) { (offset: Int, limit: Int) =>
-            ctx =>
-              (model ? ItemsWithOffset(offset, limit)) map {
-                case Items(slice) =>
-                  val q = Uri.Query("offset" -> (offset + limit).toString, "limit" -> limit.toString)
-                  val next = ctx.request.uri.copy(query = q)
-                  ctx.complete(OK, linkHeader(next) :: cacheHeader(slice) :: Nil, slice)
-              }
+    cache(simpleCache) {
+      get {
+        path(JavaUUID) { id =>
+          onSuccess(model ? id) {
+            case i: Item => complete(OK, cacheHeader(i.cache) :: Nil, i)
+            case None => complete(NotFound)
           }
         } ~
+          path("by-offset") {
+            parameters('offset ? 0, 'limit ? 3) { (offset: Int, limit: Int) =>
+              ctx =>
+                (model ? ItemsWithOffset(offset, limit)) map {
+                  case Items(slice) =>
+                    val q = Uri.Query("offset" -> (offset + limit).toString, "limit" -> limit.toString)
+                    val next = ctx.request.uri.copy(query = q)
+                    ctx.complete(OK, linkHeader(next) :: cacheHeader(slice) :: Nil, slice)
+                }
+            }
+          }
+      } ~
         path("by-date") {
           parameters('since ? 0L, 'limit ? 3) { (since, limit) =>
             ctx =>
@@ -77,6 +83,5 @@ class Service(model: ActorRef) extends HttpServiceActor with ItemJsonProtocol {
               }
           }
         }
-
     })
 }
